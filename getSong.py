30c107eb4429ca9bf5a8a39b3b0bb75d565cb8e2
Erik <erik@noreply@gmail.com>
2018-09-26 23:19:44 -0700

    v1.2

--- getSong.py
+++ getSong.py
@@ -2,44 +2,86 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 import time
+import sys
 
-noGUI = Options()
-noGUI.add_argument('--headless')
-options = webdriver.FirefoxProfile()
-options.set_preference("dom.popup_maximum", 0)
-options.set_preference("privacy.popups.showBrowserMessage", False)
-options.set_preference("browser.preferences.instantApply",True)
-options.set_preference("browser.download.folderList",0)
-options.set_preference("browser.download.manager.showWhenStarting", False)
-options.set_preference("browser.download.dir","/home/erik/Desktop/")
-options.set_preference("browser.helperApps.alwaysAsk.force",False)
-options.set_preference("browser.helperApps.neverAsk.saveToDisk","audio/mpeg")
-options.set_preference("browser.helperApps.neverAsk.openFile","audio/mpeg")
+class frameWork:
 
-browser = webdriver.Firefox(firefox_profile=options, firefox_options=noGUI)
-browser.get('https://ytmp3.cc/')
+    noGUI = None
+    options = None
+    browser = None
 
-with open('/home/erik/Documents/Programming/Python/mp3Downloader/musicUrl.txt', "r") as urls:
-    for Url in urls:
-        browser.get('https://ytmp3.cc/')
-        currentWindow = browser.window_handles[0]
-        url = browser.find_element_by_id('input')
-        url.send_keys(Url.strip())
-        convert = browser.find_element_by_id('submit')
-        convert.submit()
-        while True:
-            try:
-                download = browser.find_element_by_id('download').click()
-                #download.submit()
-                break
-            except:
-                time.sleep(5)
-        try:
-            if len(browser.window_handles) == 1:
-            else:
-                browser.switch_to.window(browser.window_handles[-1])
-                browser.close()
-                browser.switch_to.window(currentWindow)
-        except:
-            pass
-browser.close()
+    def preferences(self):
+        self.noGUI = Options()
+        self.noGUI.add_argument('--headless')
+        self.options = webdriver.FirefoxProfile()
+        self.options.set_preference(
+            "dom.popup_maximum",
+            0)
+        self.options.set_preference(
+            "privacy.popups.showBrowserMessage", 
+            False)
+        self.options.set_preference(
+            "browser.preferences.instantApply",
+            True)
+        self.options.set_preference(
+            "browser.download.folderList",
+            0)
+        self.options.set_preference(
+            "browser.download.manager.showWhenStarting", 
+            False)
+        self.options.set_preference(
+            "browser.download.dir","/home/erik/Desktop/")
+        self.options.set_preference(
+            "browser.helperApps.alwaysAsk.force",
+            False)
+        self.options.set_preference(
+            "browser.helperApps.neverAsk.saveToDisk",
+            "audio/mpeg, video/mp4")
+        self.options.set_preference(
+            "browser.helperApps.neverAsk.openFile",
+            "audio/mpeg, video/mp4")
+
+    def session(self, mode):
+        self.preferences()
+        if mode in "start":
+            self.browser = webdriver.Firefox(firefox_profile=self.options, firefox_options=self.noGUI)
+        else:
+            self.browser.close()
+
+    def download(self, type):
+        with open('/home/erik/Documents/Programming/Python/mp3Downloader/musicUrl.txt', "r") as urls:
+            for Url in urls:
+                self.browser.get('https://ytmp3.cc/')
+                currentWindow = self.browser.window_handles[0]
+                url = self.browser.find_element_by_id('input')
+                url.send_keys(Url.strip())
+                
+                if type == "mp4":
+                    self.browser.find_element_by_id('mp4').click()
+
+                convert = self.browser.find_element_by_id('submit')
+                convert.submit()
+                while True:
+                    try:
+                        self.browser.find_element_by_id('download').click()
+                        break
+                    except:
+                        time.sleep(5)
+                try:
+                    if len(self.browser.window_handles) == 1:
+                        pass
+                    else:
+                        self.browser.switch_to.window(self.browser.window_handles[-1])
+                        self.browser.close()
+                        self.browser.switch_to.window(currentWindow)
+                except:
+                    pass
+
+if len(sys.argv) < 2:
+    print('Input Error. Input mp3 or mp4 to conver to')
+    exit()
+else:
+    session = frameWork()
+    session.session('start')
+    session.download(sys.argv[1])
+    session.session('end')
